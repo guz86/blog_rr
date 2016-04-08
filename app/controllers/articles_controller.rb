5ee2067dfbd72394f7ab64bc9a849b0fbf96d9e1
Pavel Gordienko <guzmo6@rambler.ru>
2016-04-08 23:52:23 +0300

    redirect post atricle

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.valid?
       @article.save
+      redirect_to @article
     else
       render action: 'new'
     end
@@ -19,5 +20,5 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :text)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 # 1 resource :contacts, only: [:create]
 
   resource :contacts, only: [:new, :create], path_names: {:new => ''}
-  resource :articles, only: [:new, :create], path_names: {:new => ''}
+  resource :articles, only: [:new, :create]
 
   get '/terms' => 'terms#terms'
 
