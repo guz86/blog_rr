bcd1b32a6f8ea3eff6130c2faa7c4bf0d5b30691
Pavel Gordienko <guzmo6@rambler.ru>
2016-05-25 02:21:17 +0300

    add article spec expect title

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require 'support/factory_girl'
 
 describe Article do
 
@@ -10,5 +11,17 @@ describe Article do
   describe "associations" do
     it { should have_many :comments }
   end
+
+  describe "#subject" do
+    it "return the article title" do 
+      # создаем объекта article хитрым способом
+      article = create(:article, title: 'Lorem Ipsum')
+
+      # assert, проверка
+      expect(article.subject).to eq 'Lorem Ipsum'
+
+    end
+  end
+
 end 
 
