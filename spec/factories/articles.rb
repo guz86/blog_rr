0becd57e928b6a4ca3bdb04691c317f6be3e7a35
Pavel Gordienko <guzmo6@rambler.ru>
2016-05-25 02:42:13 +0300

    add test for last_comment and create factories

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,13 @@ FactoryGirl.define do
   factory :article do
     title "Article title"
     text "Article text"
+
+    # создаем фабрику для создание статьи с несколькими комментами
+    factory :article_with_comments do
+      # создаем список из 3х комментариев
+      after :create do |article, evaluator|
+        create_list :comment, 3, article: article 
+      end
+    end
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,25 +3,33 @@ require 'support/factory_girl'
 
 describe Article do
 
-  describe "validations" do
-    it { should validate_presence_of :title }
-    it { should validate_presence_of :text }
-  end
+    describe "validations" do
+      it { should validate_presence_of :title }
+      it { should validate_presence_of :text }
+    end
 
-  describe "associations" do
-    it { should have_many :comments }
-  end
+    describe "associations" do
+      it { should have_many :comments }
+    end
 
-  describe "#subject" do
-    it "return the article title" do 
+    describe "#subject" do
+      it "return the article title" do 
       # создаем объекта article хитрым способом
       article = create(:article, title: 'Lorem Ipsum')
 
       # assert, проверка
       expect(article.subject).to eq 'Lorem Ipsum'
-
+      end
     end
-  end
 
+    describe "#last_comment" do
+      it "returns the last comment" do
+        # создам статью, с комментариями
+        article = create(:article_with_comments)
+
+        # проверка
+        expect(article.last_comment.body).to eq "comment body 3"
+      end
+    end
 end 
 
