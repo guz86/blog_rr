da8ecd8f7cd72301b2c132f4fa4a82fee8730f9b
Pavel Gordienko <guzmo6@rambler.ru>
2016-06-04 08:13:24 +0300

    test comment -body length

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require "spec_helper"
 
 describe Comment do
-  it { should belong_to :article }
+
+    describe "validations" do
+      it { should validate_length_of(:body).is_at_most(4000) }
+    end
+
+    describe "associations" do
+      it { should belong_to :article }
+    end  
+  
 end 
