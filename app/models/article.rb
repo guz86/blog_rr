92dc34fd223265b75deae2b8ade4373b6bac7747
Pavel Gordienko <guzmo6@rambler.ru>
2016-06-04 08:07:55 +0300

    validate text length

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ActiveRecord::Base
   validates :title, presence: true
   validates :text, presence: true
   validates_length_of :title, maximum: 140
+  validates_length_of :text, maximum: 4000
   has_many :comments
 
   def subject
