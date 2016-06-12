2a91e1e43c6ef908b421b1978c4e5a8e4e5c0a2d
Pavel Gordienko <guzmo6@rambler.ru>
2016-06-12 16:08:37 +0300

    add session_helper and fix problem add support
     undefined local variable or method `sign_upp' for #<RSpec::ExampleGroups::AccountCreation:0x0000000af4ab28>

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -8,14 +8,3 @@ feature "Account Creation" do
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
 end
-
-def sign_upp
-      visit new_user_registration_path
-
-    fill_in :user_email, :with => 'user@example.com'
-    fill_in :user_username, :with => 'mike'
-    fill_in :user_password, :with => 'secure123$%'
-    fill_in :user_password_confirmation, :with => 'secure123$%'
-    
-    click_button 'Sign up'
-end
