1652cb9e068558869f4eaa12b6fd0ef9fa39617a
Pavel Gordienko <guzmo6@rambler.ru>
2016-06-13 12:39:38 +0300

    allows user to create new article page

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -9,4 +9,16 @@ feature "Article Creation" do
     visit new_article_path
     expect(page).to have_content I18n.t 'article.new_article'
   end
+
+  scenario "allows user to create new article page" do
+    visit new_article_path
+
+    fill_in :article_title, :with => 'new title'
+    fill_in :article_text, :with => 'something'
+
+    click_button 'Save Article'
+
+    expect(page).to have_content I18n.t 'article.article_theme'
+  end
+
 end
